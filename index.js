fde9f9c77ddd31786e7067e9af5d8ecea9cba75b
Samantha Jordan <shinyguns@google.com>
2021-03-08 12:49:08 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let finished = []
+    for (let i = 0; i < array.length; i++) {
+    finished.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return finished
+};
+
+function countDown() {
+    let countDown = 0;
+    while (countDown < 11) {
+        console.log(countDown++);
+    }
+};
+countdown(10);
